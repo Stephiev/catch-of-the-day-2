466d136bf6d755fc0b2e7e62d91d36e6c825d2bc
Stephanie Vasquez-Soltero <stephievictoria@gmail.com>
2018-01-04 13:09:34 -0800

    Comments in JSX return

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -4,8 +4,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
   render() {
+    // This is like any other comment
+    // Only when you're in the return
+    // statement is the special
+    // comment needed
     return (
       <form action="" className="store-selector">
+      { /* This is a JSX comment */ }
       <h2>Please Enter A Store</h2>
       <input type="text" required placeholder="Store Name"/>
       <button type="submit">Visit Store -></button>
