0a1a90ac62018b5b7db20e8317e2b6a83ae3d97b
Stephanie Vasquez-Soltero <stephievictoria@gmail.com>
2018-01-04 15:00:35 -0800

    Added more basic components, and a stateless functional header

---
+++ src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Inventory extends React.Component {
+  render() {
+    return (
+        <p>This is Inventory.js</p>
+    )
+  }
+}
+
+export default Inventory;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@
 // react library into the "React" variable
 import React from 'react';
 import { render } from 'react-dom'
+import './css/style.css'
 
-import StorePicker from './components/StorePicker'
+import App from './components/App'
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App/>, document.querySelector('#main'));
